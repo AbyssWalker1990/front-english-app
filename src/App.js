d7b6a6de06ddd6ad5f3d898e69c40569e6a0f737
Volodymyr Bordiian <bordiian.v@gmail.com>
2023-07-16 14:21:10 +0300

    added logout functionality to navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import { Route, Routes } from 'react-router-dom'
 import Layout from './components/Layout'
 import Public from './components/Public'
+import Login from './feature/auth/Login'
+import Welcome from './feature/auth/Welcome'
+import Register from './feature/auth/Register'
 
 function App () {
   return (
     <Routes>
       <Route path='/' element={<Layout />}>
         <Route index element={<Public />} />
+        <Route path="login" element={<Login />} />
+        <Route path="register" element={<Register />} />
+        <Route path="welcome" element={<Welcome />} />
       </Route>
     </Routes>
   )
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,15 +1,22 @@
 import { Link } from 'react-router-dom'
+import { useSendLogoutMutation } from '../feature/auth/authApiSlice'
 
 const NavBar = () => {
+  const [sendLogout, {
+    isLoading,
+    isSuccess,
+    isError,
+    error
+  }] = useSendLogoutMutation()
 
   return (
     <header className="nav-header">
       <div className="nav-header__container">
         <Link to="/">
-          <h1 className="nav-header__title">techNotes</h1>
+          <h1 className="nav-header__title">Navbar</h1>
         </Link>
         <nav className="nav-header__nav">
-          {/* add nav buttons later */}
+          <button onClick={sendLogout}>Logout</button>
         </nav>
       </div>
     </header>
