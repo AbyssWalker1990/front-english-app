beecfd3df8feebdea7bc95fcf1d817d8c66eb7d1
Volodymyr Bordiian <bordiian.v@gmail.com>
2023-07-25 19:43:58 +0300

    all info displayed on edit course page now

--- src/feature/courses/EditExercisesForm.jsx
+++ src/feature/courses/EditExercisesForm.jsx
@@ -1,8 +1,50 @@
 import React from 'react'
 
-const EditExercisesForm = ({ exercises }) => {
+const EditExercisesForm = ({ exercises, lessonPosition }) => {
+
+  const exerciseContent = exercises.map(exercise => {
+    const { exercisePos, exerciseType, exerciseDescription, exerciseQuizAnswers } = exercise
+
+    const exerciseAnswersPromo = exerciseQuizAnswers.map(answer => (
+      <div key={`radio-${answer}`}>
+        <input type="radio" id={answer.id} name={`exercise-${exercisePos}`} value={answer} />
+        <label htmlFor={answer}>{answer}</label>
+      </div>
+    ))
+
+    const exerciseAnswersEdit = exerciseQuizAnswers.map(answer => (
+      <div key={`edit-answer-${answer}`}>
+        <p>Answer Position: {exerciseQuizAnswers.indexOf(answer) + 1}</p>
+        <input type="text"
+          id={`answer-${exerciseQuizAnswers.indexOf(answer)}`}
+          defaultValue={answer} />
+      </div>
+    ))
+
+    return (
+      <div key={`key-exercise-${exercise._id}-${lessonPosition}`} id={`exercise-${exercise._id}-${lessonPosition}`} className='border padding-all'>
+        <div>
+          <h1>{exercisePos}. Type: {exerciseType}</h1>
+          <p>Exercise description: {exerciseDescription}</p>
+          < fieldset >
+            <legend>Select correct Answer:</legend>
+            {exerciseAnswersPromo}
+          </fieldset>
+        </div>
+        <div id='answer-edit'>
+          {exerciseAnswersEdit}
+        </div>
+      </div>
+    )
+  })
+
+  console.log('exerciseContent: ', typeof exerciseContent)
+
+
   return (
-    <div>EditExercisesForm</div>
+    <>
+      {exerciseContent}
+    </>
   )
 }
 
--- src/feature/courses/EditLessonForm.jsx
+++ src/feature/courses/EditLessonForm.jsx
@@ -3,37 +3,30 @@ import EditExercisesForm from "./EditExercisesForm"
 const EditLessonForm = ({ lesson }) => {
   const { lessonTitle, lessonPosition, lessonDescription, lessonExercises } = lesson
 
-  let lessonContent
-  let exerciseContent
-  if (lesson) {
+  const lessonContent = (
+    <>
+      <div className="lesson-edit-form">
+        <h1>{lessonPosition}. Title:</h1>
+        <input type="text"
+          id={`lesson-${lesson._id}-title`}
+          defaultValue={lessonTitle} />
+      </div>
+      <div className="lesson-edit-form">
+        <h1>Description: </h1>
+        <textarea
+          id={`lesson-${lesson._id}-description`}
+          rows="4"
+          cols="50"
+          defaultValue={lessonDescription}
+        />
+      </div>
+      <div className="lesson-edit-form border padding-all">
+        <h1>HomeWork: </h1>
+        <EditExercisesForm key={lessonExercises._id} exercises={lessonExercises} lessonPosition={lessonPosition} />
+      </div>
+    </>
+  )
 
-    exerciseContent = lessonExercises.map(exercise => (
-      <EditExercisesForm key={exercise._id} exercise={exercise} />
-    ))
-
-    lessonContent = (
-      <>
-        <div className="lesson-edit-form">
-          <h1>{lessonPosition}. Title:</h1>
-          <input type="text"
-            id={`lesson-${lesson._id}-title`}
-            defaultValue={lessonTitle} />
-        </div>
-        <div className="lesson-edit-form">
-          <h1>Description: </h1>
-          <textarea
-            id={`lesson-${lesson._id}-description`}
-            rows="4"
-            cols="50"
-            defaultValue={lessonDescription}
-          />
-        </div>
-        <div className="lesson-edit-form">
-          {exerciseContent}
-        </div>
-      </>
-    )
-  }
   return (
     <div id={`lesson-${lessonPosition}`} className="border vert-margin padding-all">
       {lessonContent}
