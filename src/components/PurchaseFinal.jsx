e0bff34d77469e2eec38a8ce240ca203ce771e91
Volodymyr Bordiian <bordiian.v@gmail.com>
2023-09-03 18:07:01 +0300

    added refetch and navigation to course after purchasing

--- src/components/PurchaseFinal.jsx
+++ src/components/PurchaseFinal.jsx
@@ -1,9 +1,14 @@
+import { useGetProfileQuery } from '../feature/profile/profileApiSlice'
 import { useAddAndSetActiveCourseMutation } from '../feature/profile/profileApiSlice'
 import useAuth from '../hooks/useAuth'
+import { useNavigate } from 'react-router-dom'
 
 const PurchaseFinal = () => {
   const { username } = useAuth()
   const [addAndSetActiveCourse] = useAddAndSetActiveCourseMutation()
+  const [refetch] = useGetProfileQuery()
+
+  const navigate = useNavigate()
 
   const buyCourse = async () => {
     const puchaseData = {
@@ -11,8 +16,10 @@ const PurchaseFinal = () => {
       username,
     }
     const purchase = await addAndSetActiveCourse(puchaseData)
+    await refetch()
     console.log(username)
     console.log(purchase)
+    navigate('/active-course')
   }
 
   return (
