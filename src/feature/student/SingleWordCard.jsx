052b9cbfd518cc606ba071a3ff097d500814e00c
Volodymyr Bordiian <volodymyr_bordiyan@dev.abnk.uk>
2023-09-24 12:56:50 +0300

    added reveal translation functionality on click

--- src/feature/student/SingleWordCard.jsx
+++ src/feature/student/SingleWordCard.jsx
@@ -1,8 +1,26 @@
+import { useRef } from 'react'
+
 const SingleWordCard = ({ card }) => {
   const { english, ukrainian } = card
+
+  const engRef = useRef()
+  const ukrRef = useRef()
+
+  const revealTranslation = () => {
+    engRef.current.className = 'hidden'
+    ukrRef.current.className = ''
+    setTimeout(() => {
+      engRef.current.className = ''
+      ukrRef.current.className = 'hidden'
+    }, 5000)
+  }
+
   return (
-    <article className='wordCard'>
-      <p className='englishWord'>{english}</p>
+    <article className='wordCard' onClick={revealTranslation}>
+      <p ref={engRef}>{english}</p>
+      <p ref={ukrRef} className='hidden'>
+        {ukrainian}
+      </p>
     </article>
   )
 }
