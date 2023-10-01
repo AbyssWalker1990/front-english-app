49a4b04cd13158260a64d5898f641d6a66f85711
Volodymyr Bordiian <volodymyr_bordiyan@dev.abnk.uk>
2023-10-01 13:38:58 +0300

    add possibility to add end edit quizes and solve it by students

--- src/feature/courses/edit/EditExercisesForm.jsx
+++ src/feature/courses/edit/EditExercisesForm.jsx
@@ -1,6 +1,6 @@
-import { onSelectType } from "../utils/utils"
-import { useState, useEffect } from "react"
-import { SaveButton } from "./buttons/SaveButton"
+import { onSelectType } from '../utils/utils'
+import { useState, useEffect } from 'react'
+import { SaveButton } from './buttons/SaveButton'
 
 const EditExercisesForm = ({
   exercises,
@@ -8,61 +8,85 @@ const EditExercisesForm = ({
   blockPosition,
   setCurCourse,
   updateCourse,
-  curCourse
+  curCourse,
 }) => {
   const [key, setKey] = useState(0)
 
   useEffect(() => {
-    setKey(prevKey => prevKey + 1)
+    setKey((prevKey) => prevKey + 1)
   }, [setCurCourse])
 
-
-  const exerciseContent = exercises.map(exercise => {
-    const { exercisePos, exerciseType, exerciseDescription, exerciseQuizAnswers, correctAnswer } = exercise
+  const exerciseContent = exercises.map((exercise) => {
+    const {
+      exercisePos,
+      exerciseType,
+      exerciseDescription,
+      exerciseQuizAnswers,
+      correctAnswer,
+    } = exercise
 
     const onAnswerEdit = (e) => {
-      const answersBlock = document.getElementById(`answer-edit-${exercise._id}`)
+      const answersBlock = document.getElementById(
+        `answer-edit-${exercise._id}`
+      )
       const answers = answersBlock.getElementsByTagName('input')
-      const answersArray = Array.from(answers).map(answer => answer.value)
-      console.log(answersArray)
-      setCurCourse(prevState => {
+      const splittedId = e.target.id.split('-')
+      const lessonIndex = lessonPosition - 1
+      const blockIndex = blockPosition - 1
+      const key = splittedId[splittedId.length - 1]
+      const answersArray = Array.from(answers).map((answer) => answer.value)
+      setCurCourse((prevState) => {
         const duplicate = JSON.parse(JSON.stringify(prevState))
+        duplicate.lessons[lessonIndex].exercisesBlocks[
+          blockIndex
+        ].blockExercises[key - 1].exerciseQuizAnswers = answersArray
         return duplicate
       })
     }
 
     const onExerciseChange = (e) => {
       const splittedId = e.target.id.split('-')
-      const curExercisePosition = (Number(splittedId[splittedId.length - 2]) - 1)
+      const curExercisePosition = Number(splittedId[splittedId.length - 2]) - 1
       const key = splittedId[splittedId.length - 1]
       const lessonIndex = lessonPosition - 1
       const blockIndex = blockPosition - 1
-      setCurCourse(prevState => {
+      setCurCourse((prevState) => {
         const duplicate = JSON.parse(JSON.stringify(prevState))
-        duplicate.lessons[lessonIndex].exercisesBlocks[blockIndex].blockExercises[curExercisePosition][key] = e.target.value
+        duplicate.lessons[lessonIndex].exercisesBlocks[
+          blockIndex
+        ].blockExercises[curExercisePosition][key] = e.target.value
         return duplicate
       })
     }
 
-    const exerciseAnswersPromo = exerciseQuizAnswers.map(answer => (
+    const exerciseAnswersPromo = exerciseQuizAnswers.map((answer) => (
       <div key={`radio-${answer}`}>
-        <input type="radio" id={answer.id} name={`exercise-${exercisePos}`} value={answer} />
+        <input
+          type='radio'
+          id={answer.id}
+          name={`exercise-${exercisePos}`}
+          value={answer}
+        />
         <label htmlFor={answer}>{answer}</label>
       </div>
     ))
 
-    const exerciseAnswersEdit = exerciseQuizAnswers.map(answer => (
+    const exerciseAnswersEdit = exerciseQuizAnswers.map((answer) => (
       <div key={`edit-answer-${answer}-${Math.random()}`}>
         <p>Answer Position: {exerciseQuizAnswers.indexOf(answer) + 1}</p>
-        <input type="text"
+        <input
+          type='text'
           id={`answer-${exerciseQuizAnswers.indexOf(answer)}`}
-          defaultValue={answer} />
-        <button id={`answers-${exercise._id}-${exercisePos}`} onClick={onAnswerEdit}>
+          defaultValue={answer}
+        />
+        <button
+          id={`answers-${exercise._id}-${exercisePos}`}
+          onClick={onAnswerEdit}
+        >
           Save Answers
         </button>
       </div>
     ))
-
 
     return (
       <div
@@ -71,54 +95,60 @@ const EditExercisesForm = ({
         className='border padding-all vert-margin border-orange'
       >
         <div>
-          <label htmlFor="exercise-type"> Choose type:</label>
+          <label htmlFor='exercise-type'> Choose type:</label>
           <select
             id={`exercise-type-${exercise._id}`}
-            onChange={(e) => onSelectType(e, exercise._id, setCurCourse, lessonPosition, blockPosition, exercisePos)}
+            onChange={(e) =>
+              onSelectType(
+                e,
+                exercise._id,
+                setCurCourse,
+                lessonPosition,
+                blockPosition,
+                exercisePos
+              )
+            }
           >
             <option value={exerciseType}>{exerciseType}</option>
-            <option value="quiz">quiz</option>
-            <option value="not-quiz">not-quiz</option>
+            <option value='quiz'>quiz</option>
+            <option value='not-quiz'>not-quiz</option>
           </select>
           <div>
-            <label htmlFor={`edit-${exercisePos}-exerciseDescription`}>Exercise Description: </label>
+            <label htmlFor={`edit-${exercisePos}-exerciseDescription`}>
+              Exercise Description:{' '}
+            </label>
           </div>
           <textarea
             id={`edit-${exercisePos}-exerciseDescription`}
-            rows="4"
-            cols="50"
+            rows='4'
+            cols='50'
             onChange={onExerciseChange}
             defaultValue={exerciseDescription}
           />
-
         </div>
         <div id={`quiz-options-${exercise._id}`} className='hidden'>
           {/* < fieldset >
             <legend>Select correct Answer:</legend>
             {exerciseAnswersPromo}
           </fieldset> */}
-          <div id={`answer-edit-${exercise._id}`}>
-            {exerciseAnswersEdit}
-          </div>
+          <div id={`answer-edit-${exercise._id}`}>{exerciseAnswersEdit}</div>
         </div>
 
         <div id={`correct-answer-edit-${exercise._id}`}>
           <p>Correct answer: </p>
-          <input type="text"
+          <input
+            type='text'
             id={`edit-${exercisePos}-correctAnswer`}
             onChange={onExerciseChange}
-            defaultValue={correctAnswer} />
+            defaultValue={correctAnswer}
+          />
         </div>
         <SaveButton curCourse={curCourse} updateCourse={updateCourse} />
       </div>
     )
   })
 
-  return (
-    <>
-      {exerciseContent}
-    </>
-  )
+  return <>{exerciseContent}</>
 }
 
 export default EditExercisesForm
--- src/feature/student/ExerciseList.jsx
+++ src/feature/student/ExerciseList.jsx
@@ -1,12 +1,8 @@
-
 const ExerciseList = ({ exercises, setAnswers, blockPosition }) => {
-
-
-
   let content = null
 
   const handleAnswerChange = (e, position) => {
-    setAnswers(prevState => {
+    setAnswers((prevState) => {
       console.log('prevState: ', prevState)
       const duplicate = JSON.parse(JSON.stringify(prevState))
       duplicate[position - 1].studentsAnswer = e.target.value
@@ -14,20 +10,41 @@ const ExerciseList = ({ exercises, setAnswers, blockPosition }) => {
     })
   }
 
-
   if (exercises) {
-    content = exercises.map(exercise => (
-      <div key={exercise._id} className="exercise-content-student border vert-margin padding-all">
-        <p>{exercise.exercisePos}. {exercise.exerciseDescription}</p>
-        <div className="exercise-answer-input">
-          <label htmlFor={`${exercise._id}-answer`}>Answer: </label>
-          <input
-            type="text"
-            onInput={(e) => handleAnswerChange(e, exercise.exercisePos)}
-            onChange={(e) => handleAnswerChange(e, exercise.exercisePos)}
-            id={`${exercise._id}-answer-block-${blockPosition}`}
-          />
-        </div>
+    content = exercises.map((exercise) => (
+      <div
+        key={exercise._id}
+        className='exercise-content-student border vert-margin padding-all'
+      >
+        <p>
+          {exercise.exercisePos}. {exercise.exerciseDescription}
+        </p>
+        {exercise.exerciseType === 'quiz' ? (
+          <div className='exercise-answer-input'>
+            {exercise.exerciseQuizAnswers.map((quizAnswer, index) => (
+              <p>
+                {index + 1}. {quizAnswer}
+              </p>
+            ))}
+            <label htmlFor={`${exercise._id}-answer`}>Answer: </label>
+            <input
+              type='text'
+              onInput={(e) => handleAnswerChange(e, exercise.exercisePos)}
+              onChange={(e) => handleAnswerChange(e, exercise.exercisePos)}
+              id={`${exercise._id}-answer-block-${blockPosition}`}
+            />
+          </div>
+        ) : (
+          <div className='exercise-answer-input'>
+            <label htmlFor={`${exercise._id}-answer`}>Answer: </label>
+            <input
+              type='text'
+              onInput={(e) => handleAnswerChange(e, exercise.exercisePos)}
+              onChange={(e) => handleAnswerChange(e, exercise.exercisePos)}
+              id={`${exercise._id}-answer-block-${blockPosition}`}
+            />
+          </div>
+        )}
       </div>
     ))
   }
