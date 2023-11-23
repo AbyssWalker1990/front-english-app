26a5c4a0aba1931a3f5c65f0276a62218f0e1f78
Volodymyr Bordiian <volodymyr_bordiyan@dev.abnk.uk>
2023-11-23 19:31:25 +0200

    add new img for course? not full

--- src/components/CoursesPage.jsx
+++ src/components/CoursesPage.jsx
@@ -1,351 +1,55 @@
 const CoursesPage = () => {
-  const payBasic = () => {
-    window.location.href = 'https://prt.mn/giSS50jvg9'
+  const payBasicFinancier = () => {
+    window.location.href = 'https://prt.mn/OOe_wt_fU'
   }
-  const payOptimal = () => {
-    window.location.href = 'https://prt.mn/F4e96TjiRk'
+  const payOptimalFinancier = () => {
+    window.location.href = 'https://prt.mn/42TvqoZBPM'
   }
-  const payPremium = () => {
-    window.location.href = 'https://prt.mn/MO2JeciWSL'
+  const payAdvancedFinancier = () => {
+    window.location.href = 'https://prt.mn/dP-jWa0-k'
+  }
+  const payPremiumFinancier = () => {
+    window.location.href = 'https://prt.mn/ZA7Gpw7D7'
   }
 
   return (
     <div className='coursesPageContainer'>
       <section id='courses-list-overview'>
         <article className='courseCard'>
-          <form action='https://www.portmone.com.ua/gateway/' method='post'>
-            <input type='hidden' name='payee_id' value='1185' />
-            <input
-              type='hidden'
-              name='shop_order_number'
-              value='76575j65465464161hhhh'
+          <button type='button' onClick={payBasicFinancier}>
+            <img
+              src='img/courses/corses_with_qr/financiers_basic/Eng 4 Financiers - Basic - New.jpg'
+              alt='course description'
             />
-            <input type='hidden' name='bill_amount' value='1' />
-            <input type='hidden' name='description' value='Опис замовлення' />
-            <input
-              type='hidden'
-              name='success_url'
-              value='http://example.com/success.html'
-            />
-            <input
-              type='hidden'
-              name='failure_url'
-              value='http://example.com/failure.html'
-            />
-            <input type='hidden' name='lang' value='uk' />
-            <input type='hidden' name='encoding' value='UTF-8' />
-            <input type='hidden' name='exp_time' value='400' />
-            <button type='button' onClick={payBasic}>
-              <img
-                src='img/courses/course_offer_En_4_Financiers_Basic.jpg'
-                alt='course description'
-              />
-            </button>
-          </form>
+          </button>
         </article>
 
         <article className='courseCard'>
-          <form action='https://www.portmone.com.ua/gateway/' method='post'>
-            <input type='hidden' name='payee_id' value='1185' />
-            <input
-              type='hidden'
-              name='shop_order_number'
-              value='76575j65465464161hhhh'
+          <button type='button' onClick={payOptimalFinancier}>
+            <img
+              src='img/courses/corses_with_qr/financiers_optimal/Eng 4 Financiers - Optimal - New.jpg'
+              alt='course description'
             />
-            <input type='hidden' name='bill_amount' value='1' />
-            <input type='hidden' name='description' value='Опис замовлення' />
-            <input
-              type='hidden'
-              name='success_url'
-              value='http://example.com/success.html'
-            />
-            <input
-              type='hidden'
-              name='failure_url'
-              value='http://example.com/failure.html'
-            />
-            <input type='hidden' name='lang' value='uk' />
-            <input type='hidden' name='encoding' value='UTF-8' />
-            <input type='hidden' name='exp_time' value='400' />
-            <button type='button' onClick={payOptimal}>
-              <img
-                src='img/courses/course_offer_En_4_Financiers_Optimal.jpg'
-                alt='course description'
-              />
-            </button>
-          </form>
+          </button>
         </article>
 
         <article className='courseCard'>
-          <form action='https://www.portmone.com.ua/gateway/' method='post'>
-            <input type='hidden' name='payee_id' value='1185' />
-            <input
-              type='hidden'
-              name='shop_order_number'
-              value='76575j65465464161hhhh'
+          <button type='button' onClick={payAdvancedFinancier}>
+            <img
+              src='img/courses/corses_with_qr/financiers_advanced/Eng 4 Financiers - Advanced - New.jpg'
+              alt='course description'
             />
-            <input type='hidden' name='bill_amount' value='1' />
-            <input type='hidden' name='description' value='Опис замовлення' />
-            <input
-              type='hidden'
-              name='success_url'
-              value='http://example.com/success.html'
-            />
-            <input
-              type='hidden'
-              name='failure_url'
-              value='http://example.com/failure.html'
-            />
-            <input type='hidden' name='lang' value='uk' />
-            <input type='hidden' name='encoding' value='UTF-8' />
-            <input type='hidden' name='exp_time' value='400' />
-            <button type='button' onClick={payPremium}>
-              <img
-                src='img/courses/course_offer_En_4_Financiers_Premium.jpg'
-                alt='course description'
-              />
-            </button>
-          </form>
+          </button>
         </article>
 
         <article className='courseCard'>
-          <form action='https://www.portmone.com.ua/gateway/' method='post'>
-            <input type='hidden' name='payee_id' value='1185' />
-            <input
-              type='hidden'
-              name='shop_order_number'
-              value='76575j65465464161hhhh'
+          <button type='button' onClick={payPremiumFinancier}>
+            <img
+              src='img/courses/corses_with_qr/financiers_premium/Eng 4 Financiers - Premium - New.jpg'
+              alt='course description'
             />
-            <input type='hidden' name='bill_amount' value='1' />
-            <input type='hidden' name='description' value='Опис замовлення' />
-            <input
-              type='hidden'
-              name='success_url'
-              value='http://example.com/success.html'
-            />
-            <input
-              type='hidden'
-              name='failure_url'
-              value='http://example.com/failure.html'
-            />
-            <input type='hidden' name='lang' value='uk' />
-            <input type='hidden' name='encoding' value='UTF-8' />
-            <input type='hidden' name='exp_time' value='400' />
-            <button type='button' onClick={payBasic}>
-              <img
-                src='img/courses/course_offer_En_4_IT_Basic.jpg'
-                alt='course description'
-              />
-            </button>
-          </form>
+          </button>
         </article>
-
-        <article className='courseCard'>
-          <form action='https://www.portmone.com.ua/gateway/' method='post'>
-            <input type='hidden' name='payee_id' value='1185' />
-            <input
-              type='hidden'
-              name='shop_order_number'
-              value='76575j65465464161hhhh'
-            />
-            <input type='hidden' name='bill_amount' value='1' />
-            <input type='hidden' name='description' value='Опис замовлення' />
-            <input
-              type='hidden'
-              name='success_url'
-              value='http://example.com/success.html'
-            />
-            <input
-              type='hidden'
-              name='failure_url'
-              value='http://example.com/failure.html'
-            />
-            <input type='hidden' name='lang' value='uk' />
-            <input type='hidden' name='encoding' value='UTF-8' />
-            <input type='hidden' name='exp_time' value='400' />
-            <button type='button' onClick={payOptimal}>
-              <img
-                src='img/courses/course_offer_En_4_IT_Optimal.jpg'
-                alt='course description'
-              />
-            </button>
-          </form>
-        </article>
-
-        <article className='courseCard'>
-          <form action='https://www.portmone.com.ua/gateway/' method='post'>
-            <input type='hidden' name='payee_id' value='1185' />
-            <input
-              type='hidden'
-              name='shop_order_number'
-              value='76575j65465464161hhhh'
-            />
-            <input type='hidden' name='bill_amount' value='1' />
-            <input type='hidden' name='description' value='Опис замовлення' />
-            <input
-              type='hidden'
-              name='success_url'
-              value='http://example.com/success.html'
-            />
-            <input
-              type='hidden'
-              name='failure_url'
-              value='http://example.com/failure.html'
-            />
-            <input type='hidden' name='lang' value='uk' />
-            <input type='hidden' name='encoding' value='UTF-8' />
-            <input type='hidden' name='exp_time' value='400' />
-            <button type='button' onClick={payPremium}>
-              <img
-                src='img/courses/course_offer_En_4_IT_Premium.jpg'
-                alt='course description'
-              />
-            </button>
-          </form>
-        </article>
-
-        <article className='courseCard'>
-          <form action='https://www.portmone.com.ua/gateway/' method='post'>
-            <input type='hidden' name='payee_id' value='1185' />
-            <input
-              type='hidden'
-              name='shop_order_number'
-              value='76575j65465464161hhhh'
-            />
-            <input type='hidden' name='bill_amount' value='1' />
-            <input type='hidden' name='description' value='Опис замовлення' />
-            <input
-              type='hidden'
-              name='success_url'
-              value='http://example.com/success.html'
-            />
-            <input
-              type='hidden'
-              name='failure_url'
-              value='http://example.com/failure.html'
-            />
-            <input type='hidden' name='lang' value='uk' />
-            <input type='hidden' name='encoding' value='UTF-8' />
-            <input type='hidden' name='exp_time' value='400' />
-            <button
-              type='button'
-              onClick={() =>
-                (window.location.href = 'https://prt.mn/XOcVfepYAj')
-              }
-            >
-              <img
-                src='img/courses/course_offer_En_4_Tourism_Basic.jpg'
-                alt='course description'
-              />
-            </button>
-          </form>
-        </article>
-
-        <article className='courseCard'>
-          <form action='https://www.portmone.com.ua/gateway/' method='post'>
-            <input type='hidden' name='payee_id' value='1185' />
-            <input
-              type='hidden'
-              name='shop_order_number'
-              value='76575j65465464161hhhh'
-            />
-            <input type='hidden' name='bill_amount' value='1' />
-            <input type='hidden' name='description' value='Опис замовлення' />
-            <input
-              type='hidden'
-              name='success_url'
-              value='http://example.com/success.html'
-            />
-            <input
-              type='hidden'
-              name='failure_url'
-              value='http://example.com/failure.html'
-            />
-            <input type='hidden' name='lang' value='uk' />
-            <input type='hidden' name='encoding' value='UTF-8' />
-            <input type='hidden' name='exp_time' value='400' />
-            <button type='button' onClick={payOptimal}>
-              <img
-                src='img/courses/course_offer_En_4_Tourism_Optimal.jpg'
-                alt='course description'
-              />
-            </button>
-          </form>
-        </article>
-
-        <article className='courseCard'>
-          <form action='https://www.portmone.com.ua/gateway/' method='post'>
-            <input type='hidden' name='payee_id' value='1185' />
-            <input
-              type='hidden'
-              name='shop_order_number'
-              value='76575j65465464161hhhh'
-            />
-            <input type='hidden' name='bill_amount' value='1' />
-            <input type='hidden' name='description' value='Опис замовлення' />
-            <input
-              type='hidden'
-              name='success_url'
-              value='http://example.com/success.html'
-            />
-            <input
-              type='hidden'
-              name='failure_url'
-              value='http://example.com/failure.html'
-            />
-            <input type='hidden' name='lang' value='uk' />
-            <input type='hidden' name='encoding' value='UTF-8' />
-            <input type='hidden' name='exp_time' value='400' />
-            <button type='button' onClick={payPremium}>
-              <img
-                src='img/courses/course_offer_En_4_Tourism_Premium.jpg'
-                alt='course description'
-              />
-            </button>
-          </form>
-        </article>
-        <button
-          type='button'
-          style={{
-            background: '#466e91',
-            color: '#FFFFFF',
-            fontSize: '16px',
-            height: '48px',
-            width: '320px',
-            borderRadius: '10px',
-          }}
-          onClick={payBasic}
-        >
-          Оплатити через Portmone Basic{' '}
-        </button>
-        <button
-          type='button'
-          style={{
-            background: '#466e91',
-            color: '#FFFFFF',
-            fontSize: '16px',
-            height: '48px',
-            width: '320px',
-            borderRadius: '10px',
-          }}
-          onClick={payOptimal}
-        >
-          Оплатити через Portmone Optimal{' '}
-        </button>
-        <button
-          type='button'
-          style={{
-            background: '#466e91',
-            color: '#FFFFFF',
-            fontSize: '16px',
-            height: '48px',
-            width: '320px',
-            borderRadius: '10px',
-          }}
-          onClick={payPremium}
-        >
-          Оплатити через Portmone Premium{' '}
-        </button>
       </section>
     </div>
   )
